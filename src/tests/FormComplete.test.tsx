3c27c9b4553f6a0e25933025d5b816dab42f1024
Ruxandra Anghel <ruxiang05@gmail.com>
2020-04-27 10:06:59 +0100

    Fix test typo

--- src/tests/FormComplete.test.tsx
+++ src/tests/FormComplete.test.tsx
@@ -1,10 +1,9 @@
 import React from "react";
 import renderer from "react-test-renderer";
 import { shallow } from "enzyme";
-import FormPrivFormCompleteacy from "../components/FormComplete";
+import FormComplete from "../components/FormComplete";
 
 describe("FormComplete", () => {
-
   it("should render", () => {
     const tree = renderer.create(<FormComplete />).toJSON();
     expect(tree).toMatchSnapshot();
